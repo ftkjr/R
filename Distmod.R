f34d9ae05d28b3f17ab1bd79c7b9c4056de27d8a
Fred <46111287+ftkjr@users.noreply.github.com>
2019-03-07 21:30:43 -0500

    Add files via upload
    Uploaded distmod

---
+++ Distmod.R
@@ -0,0 +1,47 @@
+# DistMod(observation)
+# Fred Kaesmann Jr
+# 2019 - 03 - 07
+# Checking for which mod of the data is most normal
+
+DistMod <- function(observation){
+  # Making sure we're dealing with numbers
+  observation <- as.numeric(observation)
+  
+  # Original data
+  obsiqr <- IQR(observation) #see below
+  obssd <- sd(observation)
+  obscalc <- obsiqr/obssd
+  obsrow <- c("Original:", obscalc)
+  hist(observation) 
+  
+  # Square root of the observations
+  squirt <- sqrt(observation) # Take Square Root
+  sqiqr <- IQR(squirt)  # Inter Quartile Range
+  sqsd <- sd(squirt)  # Standard Deviation
+  sqcalc <- sqiqr/sqsd # IQR/SD
+  sqrow <- c("Square root:",sqcalc) # labeled and stored
+  hist(squirt) # Histogram
+  
+  # Log of the observations
+  lagdat <- log(observation) # Take the log
+  lagiqr <- IQR(lagdat) # Inter Quartile Range
+  lagsd <- sd(lagdat) # Standard Deviation
+  lagcalc <- lagiqr/lagsd # IQR/SD
+  lagrow <-  c("Log:", lagcalc) # labeled and stored
+  hist(lagdat)
+  
+  # Determining which version is most normal
+  if (abs(1.34-obscalc)<abs(1.34-sqcalc) & abs(1.34-obscalc)<abs(1.34-lagcalc)){
+    print("Original data is most normal")
+  } else if (abs(1.34-sqcalc)<abs(1.34-obscalc) & abs(1.34-sqcalc)<abs(1.34-lagcalc)) {
+    print("Square rooted data is most normal")
+  } else {
+    print("Log of data is most normal")
+  }
+  
+  # Group everything as a dataframe
+  thegroup <- as.data.frame(rbind(obsrow,sqrow,lagrow))
+  
+  # Print all nice like
+  print(thegroup, row.names = F)
+}
